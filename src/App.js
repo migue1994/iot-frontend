f985661b06296db38e4559f1ee341793f3faf477
migue1994 <migue24co@gmail.com>
2020-10-23 23:03:35 -0500

    front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Login from "./components/login";
+import AppContent from "./components/appContent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+    useEffect(() => {
+        const mock = {
+            username: 'admin123',
+            password: 'admin'
+        }
+        localStorage.setItem('memory', JSON.stringify(mock));
+    }, [])
+
+    return(
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/app-content" component={AppContent}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/login.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from "react";
+import "./login.css";
+import {useHistory} from 'react-router-dom';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+export default function Login(){
+
+    const history = useHistory();
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        if (JSON.parse(localStorage.getItem('user'))){
+            history.push('/app-content')
+        }
+    },[history])
+
+    function handleChange(event){
+        setUser({...user, [event.target.name] : event.target.value});
+    }
+
+    function submit(){
+        const memory = JSON.parse(localStorage.getItem('memory'));
+        if (memory.username === user.username && memory.password === user.password){
+            alert(`Welcome ${user.username}`);
+            localStorage.setItem('user', JSON.stringify(user));
+            history.push('/app-content');
+        }else {
+            alert('Bad credentials, try again')
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <div className="card">
+                <div className="avatar">
+                    {/*avatar image*/}
+                </div>
+                <h1>Login</h1>
+                <form onSubmit={submit}>
+                    <TextField
+                        label={'username'}
+                        placeholder={'Set your username'}
+                        name={'username'}
+                        onChange={handleChange}
+                        fullWidth
+                    />
+                    <TextField
+                        label={'password'}
+                        placeholder={'Set your password'}
+                        name={'password'}
+                        type="password"
+                        onChange={handleChange}
+                        fullWidth
+                    />
+                    <Button type={"submit"} fullWidth variant="contained" color={"primary"} className="button">
+                        LOGIN
+                    </Button>
+                </form>
+            </div>
+        </div>
+    )
+}
